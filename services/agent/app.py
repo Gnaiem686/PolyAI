418f6290ba72d85ab46141a18cace6fe066354dc
Mohammad Ganaiem <gnaiem686@gmail.com>
2026-06-21 17:56:10 +0300

    add prod url

--- services/agent/app.py
+++ services/agent/app.py
@@ -110,7 +110,7 @@ app = FastAPI(title="Vision Agent")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://54.225.154.109:3000"],
+    allow_origins=["http://localhost:3000", "http://54.225.154.109:3000", "http://3.218.178.230:3000"],
     allow_methods=["POST", "GET"],
     allow_headers=["Content-Type"],
 )
